66c38bf86746dd993c192bf559ef56d1d5e1e397
Mr_Rahmat <vovacet12@mail.ru>
2020-04-20 12:51:37 +0800

    era

---
+++ src/app/components/app.jsx
@@ -0,0 +1,13 @@
+// src/components/app.jsx
+
+// Import dependencies
+import React from 'react'
+import Input_m from './input_part/input_m.jsx'
+
+// Create main App component
+const App = () => (
+    <Input_m/>
+)
+
+// Export the App component
+export default App
