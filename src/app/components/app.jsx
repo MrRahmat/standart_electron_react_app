b09438444df65395c59d5e313a0ae0c19580f3e3
Mr_Rahmat <vovacet12@mail.ru>
2020-06-21 19:59:14 +0800

    Initial commit.

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -1,13 +1,17 @@
 // src/components/app.jsx
 
 // Import dependencies
-import React from 'react'
-import Input_m from './input_part/input_m.jsx'
+import React from 'react';
+import Hello from './hello/Hello.jsx';
 
 // Create main App component
-const App = () => (
-    <Input_m/>
-)
+function App() {
+    return(
+        <div>
+            <Hello/>
+        </div>
+    );
+} 
 
 // Export the App component
-export default App
+export default App;
